b5ac9060ba5ee3cae947360cd0c5fd00dbe17805
gambucugabby <39312104+gambucugabby@users.noreply.github.com>
2018-05-16 16:17:08 -0500

    Add files via upload
    Next Update...Need to do the sort algoritim, and the return.  Messing with lambda function in python sort to make it work.

--- README.txt
+++ README.txt
@@ -1,2 +1,1 @@
-This is a ReadMe.  There's not much to read here, but everything needs a ReadMe.
-So Read Me
+Python 2.7.10
--- grate.py
+++ grate.py
@@ -9,9 +9,20 @@ import time
 import shutil
 import Erlog
 
-
 def ProcFiles(filename):
+    sortaDb = []
+    i=0
     with open(filename) as f:
-        lines = f.readlines()
-
-    print lines
+        for line in f:
+            if ',' in line:
+                lines = line.split(',')
+            elif '|' in line:
+                lines = line.split('|')
+            elif ' ' in line:
+                lines = line.split(' ')
+            else:
+                ErrLogging (0, 'Invalid separator for line: ' + line)
+            sortaDb.insert(i,lines)
+            i=i+1
+    print sortaDb
+    print sorted(sortaDb, key=lambda l:l[0])
